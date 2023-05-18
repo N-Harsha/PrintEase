0f1cafe6558b7aa8d125ffac7eb21c4de3989cde
N-Harsha <harsha0770@gmail.com>
2023-05-18 21:03:55 +0530

    updated the customerController and added the authorization

--- src/main/java/com/printease/application/controller/CustomerController.java
+++ src/main/java/com/printease/application/controller/CustomerController.java
@@ -5,6 +5,7 @@ import com.printease.application.dto.ServiceProviderDto;
 import com.printease.application.service.CustomerService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -16,15 +17,18 @@ import java.util.List;
 public class CustomerController {
     private final CustomerService customerService;
     @GetMapping
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<List<ServiceProviderDto>> getFavoriteServiceProvider(Principal principal){
         return customerService.getFavoriteServiceProvider(principal.getName());
     }
     @PostMapping("favorite/{serviceProviderId}")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<MessageWrapperDto> addFavoriteServiceProvider(Principal principal, @PathVariable Long serviceProviderId){
         return customerService.addFavoriteServiceProvider(principal.getName(), serviceProviderId);
     }
 
     @DeleteMapping("favorite/{serviceProviderId}")
+    @PreAuthorize("hasRole('ROLE_CUSTOMER')")
     public ResponseEntity<MessageWrapperDto> deleteFavoriteServiceProvider(Principal principal,@PathVariable Long serviceProviderId){
         return customerService.deleteFavoriteServiceProvider(principal.getName(), serviceProviderId);
     }
